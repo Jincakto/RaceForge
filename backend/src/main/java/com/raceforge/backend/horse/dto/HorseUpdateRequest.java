9fc465aec4f677e88e217ae7a649f96430b2f145
vudangkhoa-art <vudangkhoa1110@gmail.com>
2026-09-22 22:36:46 +0700

    Update HorseUpdateRequest.java

--- backend/src/main/java/com/raceforge/backend/horse/dto/HorseUpdateRequest.java
+++ backend/src/main/java/com/raceforge/backend/horse/dto/HorseUpdateRequest.java
@@ -15,5 +15,59 @@ public class HorseUpdateRequest {
   public HorseUpdateRequest() {
   }
 
-  
+  public String getHorseName() {
+      return horseName;
+  }
+
+  public void setHorseName(String horseName) {
+      this.horseName = horseName;
+  }
+
+  public LocalDate getDateOfBirth() {
+      return dateOfBirth;
+  }
+
+  public void setDateOfBirth(LocalDate dateOfBirth) {
+      this.dateOfBirth = dateOfBirth;
+  }
+
+  public String getGender() {
+      return gender;
+  }
+
+  public void setGender(String gender) {
+      this.gender = gender;
+  }
+
+  public String getBreed() {
+      return breed;
+  }
+
+  public void setBreed(String breed) {
+      this.breed = breed;
+  }
+
+  public String getSire() {
+      return sire;
+  }
+
+  public void setSire(String sire) {
+      this.sire = sire;
+  }
+
+  public String getDam() {
+      return dam;
+  }
+
+  public void setDam(String dam) {
+      this.dam = dam;
+  }
+
+  public Double getWeight() {
+      return weight;
+  }
+
+  public void setWeight(Double weight) {
+      this.weight = weight;
+  }
 }

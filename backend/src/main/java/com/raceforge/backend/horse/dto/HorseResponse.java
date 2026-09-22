b9c01aeb85adff4d11c7219145899fed3f538be2
vudangkhoa-art <vudangkhoa1110@gmail.com>
2026-09-22 22:41:37 +0700

    Update HorseResponse.java

--- backend/src/main/java/com/raceforge/backend/horse/dto/HorseResponse.java
+++ backend/src/main/java/com/raceforge/backend/horse/dto/HorseResponse.java
@@ -20,5 +20,84 @@ public class HorseResponse {
   public HorseResponse() {
   }
 
+  public String getHorseName() {
+      return horseName;
+  }
+
+  public void setHorseName(String horseName) {
+      this.horseName = horseName;
+  }
+
+  public LocalDate getDateOfBirth() {
+      return dateOfBirth;
+  }
+
+  public void setDateOfBirth(LocalDate dateOfBirth) {
+      this.dateOfBirth = dateOfBirth;
+  }
+
+  public String getGender() {
+      return gender;
+  }
+
+  public void setGender(String gender) {
+      this.gender = gender;
+  }
+
+  public String getBreed() {
+      return breed;
+  }
+
+  public void setBreed(String breed) {
+      this.breed = breed;
+  }
+
+  public String getSire() {
+      return sire;
+  }
+
+  public void setSire(String sire) {
+      this.sire = sire;
+  }
+
+  public String getDam() {
+      return dam;
+  }
+
+  public void setDam(String dam) {
+      this.dam = dam;
+  }
+
+  public Double getWeight() {
+      return weight;
+  }
+
+  public void setWeight(Double weight) {
+      this.weight = weight;
+  }
+
+  public String getHealthStatus(){
+    return healthStatus;
+  }
+
+  public void setHealthStatus(String healthStatus) {
+    this.healthStatus = healthStatus;
+  }
+
+  public Boolean getTrainingLocked() {
+      return trainingLocked;
+  }
+
+  public void setTrainingLocked(Boolean trainingLocked) {
+      this.trainingLocked = trainingLocked;
+  }
+
+  public String getStatus() {
+      return status;
+  }
+
+  public void setStatus(String status) {
+      this.status = status;
+  }
   
 }
